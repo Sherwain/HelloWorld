a038c6c4899bbfac33a127231d51cd189adef4ae
Sherwain <sherwainwilliamson@yahoo.com>
2015-10-16 20:58:53 -0700

    Add Hello World

---
+++ src/test/HelloWorld.java
@@ -0,0 +1,9 @@
+package test;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println(6*6 / 2 * 3 + 4);
+		System.out.println(2 * 3 / 4);
+		System.out.println(2 / 3 * 4);
+	}
+}
